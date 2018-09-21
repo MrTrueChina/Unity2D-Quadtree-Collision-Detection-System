790d10c4fcb4ab062b613db66a18abb0cc5fbb94
Mr.True.China <mr.true.china@gmail.com>
2018-09-21 16:37:53 +0900

    Step-0
    第一步完成版

--- Assets/Step/0_QuadtreeBasic/QuadtreeBasic.cs
+++ Assets/Step/0_QuadtreeBasic/QuadtreeBasic.cs
@@ -1,6 +1,28 @@
-﻿using System.Collections;
+﻿/*
+ *  碰撞检测听起来很高端实际上一点科技含量都没有。
+ *  用圆形碰撞器举例：碰撞检测就是计算两个碰撞器的圆心距离和半径和哪个大，半径和大就说明碰撞了。
+ *  如果是更复杂的碰撞器则可能需要计算边的碰撞，但原理上都逃不脱一个步骤：把所有碰撞器暴力遍历一遍。
+ *  
+ *  但是纯暴力遍历会有一个很大的问题：假设碰撞器太多了遍历计算量太大怎么办？
+ *  于是一种新的思路出现了：先找出可能发生碰撞的碰撞器，之后再遍历。
+ *  四叉树就是基于这种思路产生的。
+ *  
+ *  四叉树通过将空间划分为一个个的小区域来逐步找到真正可能发生碰撞的碰撞器。
+ *  四叉树的原理核心在于“分割”：当一个节点的空间里有过多的碰撞器时，就把这个节点分成四个子节点，每个子节点拥有父节点1/4的空间，这个空间里的碰撞器也传给子节点，这样需要遍历的碰撞器就会变少。
+ *  
+ *  
+ *  这个脚本里的是最基础的四叉树，由节点和叶子两部分构成。
+ *  节点是四叉树的每个分支，节点组成树本身。
+ *  叶子是碰撞器在树立的映射，树通过叶子判断是否发生碰撞。
+ *  
+ *  除了这个脚本还有一个配合的碰撞器脚本，用来挂载到物体上。
+ *  
+ *  但需要注意的是有这两个脚本是不够的，缺少一个初始化四叉树的脚本，这两个脚本仅用来帮助理解最后的三个完成品脚本： Quadtree、QuadtreeCollider、QuadtreeObject
+ */
+
 using System.Collections.Generic;
 using UnityEngine;
+
 
 public class QuadtreeBasicLeaf<T>
 {
@@ -57,18 +79,17 @@ public class QuadtreeBasic<T>
     public void SetLeaf(QuadtreeBasicLeaf<T> leaf)
     {
         if (DontHaveChildren())
-        {
-            _leafs.Add(leaf);
-
-            if (_leafs.Count > _splitNodesNomber && _rect.width > _minWidth && _rect.height > _minHeight)
-                Split();
-        }
+            SetLeafToSelf(leaf);
         else
-        {
             SetLeafToChildren(leaf);
-        }
     }
+    void SetLeafToSelf(QuadtreeBasicLeaf<T> leaf)
+    {
+        _leafs.Add(leaf);
 
+        if (_leafs.Count > _splitNodesNomber && _rect.width > _minWidth && _rect.height > _minHeight)
+            Split();
+    }
     void SetLeafToChildren(QuadtreeBasicLeaf<T> leaf)
     {
         if (_upperRightChild._rect.pointToRectDistance(leaf.position) == 0)
@@ -103,6 +124,30 @@ public class QuadtreeBasic<T>
         foreach (QuadtreeBasicLeaf<T> leaf in _leafs)
             SetLeafToChildren(leaf);
         _leafs = null;
+    }
+
+
+    public T[] CheckCollision(Vector2 checkPosition, float checkRadius)
+    {
+        List<T> objs = new List<T>();
+        if (DontHaveChildren())
+        {
+            foreach (QuadtreeBasicLeaf<T> leaf in _leafs)
+                if (Vector2.Distance(checkPosition, leaf.position) <= checkRadius)
+                    objs.Add(leaf.obj);
+        }
+        else
+        {
+            if (_upperRightChild._rect.pointToRectDistance(checkPosition) <= checkRadius)
+                objs.AddRange(_upperRightChild.CheckCollision(checkPosition, checkRadius));
+            if (_lowerRightChild._rect.pointToRectDistance(checkPosition) <= checkRadius)
+                objs.AddRange(_lowerRightChild.CheckCollision(checkPosition, checkRadius));
+            if (_lowerLeftChild._rect.pointToRectDistance(checkPosition) <= checkRadius)
+                objs.AddRange(_lowerLeftChild.CheckCollision(checkPosition, checkRadius));
+            if (_upperLeftChild._rect.pointToRectDistance(checkPosition) <= checkRadius)
+                objs.AddRange(_upperLeftChild.CheckCollision(checkPosition, checkRadius));
+        }
+        return objs.ToArray();
     }
 
 
