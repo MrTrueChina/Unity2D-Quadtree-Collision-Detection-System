27afbc60c8390ce9063a028e40d3e502c500643f
Sun Yang1 <suny1@sdb01.com>
2019-09-02 15:21:26 +0800

    2019.9.2

--- Assets/Quadtree Collider Detection/Detectors/QuadtreeCollisionDetector.cs
+++ Assets/Quadtree Collider Detection/Detectors/QuadtreeCollisionDetector.cs
@@ -43,7 +43,7 @@ namespace MtC.Tools.QuadtreeCollider
             CircleQuadtreeCollider circleColliderA = (CircleQuadtreeCollider)colliderA;
             CircleQuadtreeCollider circleColliderB = (CircleQuadtreeCollider)colliderB;
 
-            return Vector2.Distance(circleColliderA.position, circleColliderB.position) < circleColliderA.radius + circleColliderB.radius;
+            return Vector2.Distance(circleColliderA.position, circleColliderB.position) <= circleColliderA.radius + circleColliderB.radius;
             //TODO：圆形碰撞器的半径和最大检测半径是一样的，如果功能无误可以考虑不进行强转节约计算量
         }
     }
