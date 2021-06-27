855bff90954e7224c69f4bdf5a5a3887b85d9667
Mr.ture.China <mr.true.china@gmail.com>
2021-06-27 11:35:46 +0800

    四叉树实例组件添加注释

--- Assets/Quadtree Collider Detection/QuadtreeCollider/Quadtree/Quadtree/Quadtree.cs
+++ Assets/Quadtree Collider Detection/QuadtreeCollider/Quadtree/Quadtree/Quadtree.cs
@@ -3,9 +3,8 @@ using UnityEngine;
 
 namespace MtC.Tools.QuadtreeCollider
 {
-    // 实例部分
     /// <summary>
-    /// 四叉树包装类
+    /// 四叉树实例部分
     /// </summary>
     internal partial class Quadtree : MonoBehaviour
     {
@@ -20,29 +19,47 @@ namespace MtC.Tools.QuadtreeCollider
 
         private void Awake()
         {
-            _root = new QuadtreeNode(QuadtreeConfig.startArea); // 节点创建过程中使用了Resources.Load，这个方法不能通过类的字段声明时赋值来调用
+            // 节点创建过程中使用了Resources.Load，这个方法不能通过类的字段声明时赋值来调用
+            _root = new QuadtreeNode(QuadtreeConfig.startArea);
         }
 
         private void Update()
         {
+            // 更新四叉树
             UpdateQuadtree();
 
+            // 进行检测
             Detect();
         }
 
+        /// <summary>
+        /// 更新四叉树
+        /// </summary>
         private void UpdateQuadtree()
         {
+            // 从根节点开始更新四叉树
             _root.Update();
         }
 
+        /// <summary>
+        /// 进行碰撞检测
+        /// </summary>
         private void Detect()
         {
-            List<QuadtreeCollider> detectors = new List<QuadtreeCollider>(_detectors); // 防止在进行检测时发生检测器列表的变化，直接用碰撞器列表内容创建新列表
+            // 防止在进行检测时发生检测器列表的变化，直接用碰撞器列表内容创建新列表
+            List<QuadtreeCollider> detectors = new List<QuadtreeCollider>(_detectors);
+
             foreach (QuadtreeCollider detector in detectors)
             {
+                // 获取所有与当前遍历到的碰撞器发生碰撞的碰撞器
                 List<QuadtreeCollider> collisionColliders = instance._root.GetCollidersInCollision(detector);
+
+                // 移除当前遍历的碰撞器本身
                 collisionColliders.Remove(detector);
-                detector.SendCollision(collisionColliders); //TODO：如果在检测时报出空异常等异常，可能是这里没有进行空异常的判断导致的
+
+                // 发出碰撞事件
+                detector.SendCollision(collisionColliders);
+                // XXX：如果在检测时报出空异常等异常，可能是这里没有进行空异常的判断导致的
             }
         }
     }
