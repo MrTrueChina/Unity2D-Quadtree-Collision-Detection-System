209321c10b0ff60b487df78183dd9f3a07fd3295
Mr.ture.China <mr.true.china@gmail.com>
2021-07-06 16:22:44 +0800

    更新警告文本

--- Assets/Quadtree Collider Detection/QuadtreeCollider/Colliders/Editor/QuadtreeColliderEditor.cs
+++ Assets/Quadtree Collider Detection/QuadtreeCollider/Colliders/Editor/QuadtreeColliderEditor.cs
@@ -12,7 +12,7 @@ namespace MtC.Tools.QuadtreeCollider
     [CanEditMultipleObjects]
     public class QuadtreeColliderEditor : Editor
     {
-        private static string AUTO_SUBSCRIBE_WARNING_TEXT = "自动订阅功能只能用于从对象实例化时就挂载的实现了四叉树碰撞事件接口的组件，这是因为自动订阅的碰撞器在Awake遍历所有组件并进行唯一一次订阅，并且不会自动取消订阅。在对象实例化后挂载的组件不会自动订阅。虽然不会自动取消订阅，但由于 UnityEvent 弱引用的特性不会导致内存泄漏。";
+        private static string AUTO_SUBSCRIBE_WARNING_TEXT = "自动订阅功能只能用于从对象实例化时就挂载的实现了四叉树碰撞事件接口的组件，这是因为自动订阅的碰撞器在 Awake 时遍历所有组件并进行唯一一次订阅，并且不会自动取消订阅。在对象实例化后挂载的组件不会自动订阅。虽然不会自动取消订阅，但由于 UnityEvent 弱引用的特性不会导致内存泄漏。";
 
         public override void OnInspectorGUI()
         {
