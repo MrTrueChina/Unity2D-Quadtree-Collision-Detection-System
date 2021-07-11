cbf13438372c80a74603e165018ac6699d4aca17
Mr.ture.China <mr.true.china@gmail.com>
2021-07-11 10:37:02 +0800

    添加返回值对象

---
+++ Assets/Quadtree Collider Detection/QuadtreeCollider/Quadtree/Quadtree/Result.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MtC.Tools.QuadtreeCollider
+{
+    internal partial class QuadtreeNode
+    {
+        /// <summary>
+        /// 操作的返回对象
+        /// </summary>
+        internal class OperationResult
+        {
+            /// <summary>
+            /// 操作成功
+            /// </summary>
+            public bool Success { private set; get; }
+            /// <summary>
+            /// 操作后影响到的碰撞器到节点的映射表
+            /// </summary>
+            public Dictionary<QuadtreeCollider, QuadtreeNode> CollidersToNodes { get; set; } = new Dictionary<QuadtreeCollider, QuadtreeNode>();
+
+            public OperationResult(bool success)
+            {
+                Success = success;
+            }
+        }
+    }
+}
