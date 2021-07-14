753c55ba305d76979cb46a53f0de283afb4ed056
Mr.ture.China <mr.true.china@gmail.com>
2021-07-14 15:30:33 +0800

    记录思路

--- Assets/Quadtree Collider Detection/QuadtreeCollider/Quadtree/QuadtreeNode/RemoveCollider.cs
+++ Assets/Quadtree Collider Detection/QuadtreeCollider/Quadtree/QuadtreeNode/RemoveCollider.cs
@@ -12,7 +12,7 @@ namespace MtC.Tools.QuadtreeCollider
         /// </summary>
         /// <param name="collider"></param>
         /// <returns></returns>
-        public OperationResult RemoveColliderFromSelf(QuadtreeCollider collider)
+        internal OperationResult RemoveColliderFromSelf(QuadtreeCollider collider)
         {
             bool listResult = colliders.Remove(collider);
 
@@ -31,6 +31,30 @@ namespace MtC.Tools.QuadtreeCollider
                 // 返回移除失败，移除失败不改变映射表，直接返回失败即可
                 return new OperationResult(false);
             }
+        }
+
+        /*
+         * UNDONE：合并逻辑和分割有很大不同，合并逻辑在末梢是不可能进行的，实际上进行合并逻辑的是树枝，当然这是一个向上递归的逻辑，他可以从末梢开始调用
+         * 
+         * 此外合并逻辑并不是在每次移除的时候都需要进行
+         * 在一般的移除节点逻辑中，节点移除后就可以进行合并
+         * 但在位置更新时是不可以的，如果在位置更新时发生了合并。触发合并的时候必然在更新末梢节点，合并会导致树枝成为树梢，这就导致之前树枝的更新错误，他应该按照树梢的方式更新
+         * 但在位置更新时又是必须进行合并的，否则很可能导致很多节点应该合并却没有合并，因此最好的办法是在一个节点更新完毕后再进行合并
+         * 
+         * 按照逻辑来说，如果一个节点需要合并，那么它的所有子节点也应该合并，那么在从下到上不漏合并的情况下，一个需要合并的节点需要统计碰撞器数量时只需要找他的四个子节点即可
+         * 那么可以写一个递归的统计数量方法，虽然是递归的，但实际上大约只需要查一层，不会有很大的性能损失，还能应对意外的合并不完整的情况
+         * 
+         * 那么现在看来，需要的是一个由树枝调用的合并方法，之后是一个基于这个方法的向上递归直到不能合并的方法，这两个方法都需要返回映射表的变更
+         */
+
+        private bool NeedMerge()
+        {
+
+        }
+
+        private OperationResult Merge()
+        {
+
         }
     }
 }
